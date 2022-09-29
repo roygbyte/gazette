eb79aa105588315f62f2968ad5d17f1220679db2
Scarlett <social@scarlettmcallister.com>
2022-09-28 21:44:48 -0300

    Add template for HTML documents

---
+++ resources/htmldocument/template.lua
@@ -0,0 +1,15 @@
+local Template = {}
+
+Template.HTML = [[<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+  <title>%s</title>
+</head>
+<body>
+<h1>%s</h1>
+<main>
+%s
+</main>
+</body>
+</html>]]
+
+return Template
