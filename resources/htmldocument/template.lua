dd5cefd7537bec84ae3d54c79b1190eae9c61c7b
Scarlett <social@scarlettmcallister.com>
2022-10-13 21:27:44 -0300

    Add header and footer to HTML template

--- resources/htmldocument/template.lua
+++ resources/htmldocument/template.lua
@@ -5,10 +5,14 @@ Template.HTML = [[<html xmlns="http://www.w3.org/1999/xhtml">
   <title>%s</title>
 </head>
 <body>
+<header>
 <h1>%s</h1>
+</header>
 <main>
 %s
 </main>
+<footer>
+</footer>
 </body>
 </html>]]
 
